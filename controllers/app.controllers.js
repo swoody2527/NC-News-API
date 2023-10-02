8f377672821b7542d414f4478a3a4c836e04424c
swoody2527 <swoodall2527@gmail.com>
2023-10-02 14:57:03 +0100

    added /api endpoint providing information of all endpoints

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,9 +1,16 @@
-const { fetchTopics } = require("../models/app.models.js")
+const { fetchTopics, fetchEndpoints } = require("../models/app.models.js")
 
 
 exports.getTopics = (req, res, next) => {
     fetchTopics()
     .then((topics) => res.status(200).send({ topics }))
+    .catch((err) => {
+    })
+}
+
+exports.getEndpoints = (req, res, next) => {
+    fetchEndpoints()
+    .then((endpoints) => res.status(200).send({ endpoints }))
     .catch((err) => {
     })
 }
