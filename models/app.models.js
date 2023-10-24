3f85d0e63077a7e3ef5fbbe46783b600f97ad710
swoody2527 <swoodall2527@gmail.com>
2023-10-24 15:47:44 +0100

    fetchComments bug, used articles.votes instead of comments

--- models/app.models.js
+++ models/app.models.js
@@ -61,7 +61,7 @@ const fetchCommentsByArticleId = (article_id) => {
   return fetchArticleById(article_id) //CHECKS IF ARTICLE EXISTS
     .then((response) => {
       return database.query(
-        `SELECT comment_id, articles.votes, comments.created_at, comments.author, comments.body, articles.article_id FROM comments 
+        `SELECT comment_id, comments.votes, comments.created_at, comments.author, comments.body, articles.article_id FROM comments 
     JOIN articles ON comments.article_id = articles.article_id WHERE articles.article_id = $1 ORDER BY comments.created_at DESC;`,
         [article_id]
       );
